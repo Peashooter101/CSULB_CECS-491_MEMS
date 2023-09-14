7c82c33e88393addd52d7d7f86a5b9c695dbad76
Peashooter101 <Peashooter101@yahoo.com>
2023-09-14 11:01:16 -0700

    DatabaseConnectionWindow

---
+++ DatabaseConnectionWindow.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Windows.Forms;
+
+namespace MEMS
+{
+    public partial class DatabaseConnectionWindow : Form
+    {
+        public DatabaseConnectionWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            Console.Out.WriteLine(this.dbConnectString.Text);
+            Console.Out.WriteLine(this.dbName.Text);
+            throw new System.NotImplementedException();
+        }
+        
+    }
+}
