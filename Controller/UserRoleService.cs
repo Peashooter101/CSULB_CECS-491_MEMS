0bcd6cb9fca02237854e71ac144aaa9747dd6673
Drink-some-water <106268016+Drink-some-water@users.noreply.github.com>
2023-09-20 17:01:26 -0700

    one more service and an assembly typo

--- Controller/UserRoleService.cs
+++ Controller/UserRoleService.cs
@@ -3,26 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 
 namespace MEMS
 {
     public class UserRoleService
     {
-        private readonly DatabaseContext _dbContext;
+            private readonly DatabaseContext _dbContext;
 
-        public UserRoleService(DatabaseContext _dbContext)
-        {
-            this._dbContext = _dbContext;
-        }
+            public UserRoleService(DatabaseContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
 
-        public void CreateUserRole()
-        {
+            public void CreateUserRole(string roleTitle, ObjectId client)
+            {
+                var userRoleRepository = 
+                    new Repository<UserRole>(_dbContext.database, "UserRoles");
+                var newUserRole = new UserRole()
+                {
+                    title = roleTitle,
+                    clientId = client
+                };
+                userRoleRepository.Create(newUserRole);
+            }
+
+            public void AddUser(UserRole role, List<ObjectId> locationIds, string username, string email, string password)
+            {
+                var userRoleRepository = 
+                    new Repository<UserRole>(_dbContext.database, "UserRoles");
+                var newUser = new UserRole.User()
+                {
+                    locationIds = locationIds,
+                    username = username,
+                    email = email,
+                    password = password
+                };
             
-        }
-
-        public void AddUser()
-        {
-            
-        }
+                role.users.Add(newUser);
+                userRoleRepository.Update(role);
+            }
     }
 }
