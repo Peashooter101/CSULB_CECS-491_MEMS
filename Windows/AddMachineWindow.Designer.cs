9facc91295cb78f282ede88d4eb4da8ab9ed6660
Lorenzo Murillo IV <Lorenzo.murillo01@student.csulb.edu>
2023-10-15 22:09:38 -0700

    New Window working
    Need to ensure that only one entry is possible.

--- Windows/AddMachineWindow.Designer.cs
+++ Windows/AddMachineWindow.Designer.cs
@@ -46,110 +46,100 @@ namespace MEMS
             this.clearBtn = new System.Windows.Forms.Button();
             this.lblConfirm = new System.Windows.Forms.Label();
             this.detailBox = new System.Windows.Forms.ListBox();
+            this.serialNumBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblName
             // 
-            this.lblName.Location = new System.Drawing.Point(17, 66);
-            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Location = new System.Drawing.Point(14, 46);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(109, 18);
+            this.lblName.Size = new System.Drawing.Size(82, 15);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Machine Name";
             // 
             // lblModel
             // 
-            this.lblModel.Location = new System.Drawing.Point(19, 98);
-            this.lblModel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblModel.Location = new System.Drawing.Point(14, 72);
             this.lblModel.Name = "lblModel";
-            this.lblModel.Size = new System.Drawing.Size(53, 15);
+            this.lblModel.Size = new System.Drawing.Size(40, 12);
             this.lblModel.TabIndex = 1;
             this.lblModel.Text = "Model";
             // 
             // lblManu
             // 
-            this.lblManu.Location = new System.Drawing.Point(17, 130);
-            this.lblManu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblManu.Location = new System.Drawing.Point(12, 124);
             this.lblManu.Name = "lblManu";
-            this.lblManu.Size = new System.Drawing.Size(96, 21);
+            this.lblManu.Size = new System.Drawing.Size(72, 17);
             this.lblManu.TabIndex = 2;
             this.lblManu.Text = "Manufacturer";
             // 
             // lblZone
             // 
-            this.lblZone.Location = new System.Drawing.Point(19, 194);
-            this.lblZone.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblZone.Location = new System.Drawing.Point(14, 176);
             this.lblZone.Name = "lblZone";
-            this.lblZone.Size = new System.Drawing.Size(45, 20);
+            this.lblZone.Size = new System.Drawing.Size(34, 16);
             this.lblZone.TabIndex = 3;
             this.lblZone.Text = "Zone";
             // 
             // nameBox
             // 
             this.nameBox.BackColor = System.Drawing.SystemColors.Window;
-            this.nameBox.Location = new System.Drawing.Point(135, 60);
-            this.nameBox.Margin = new System.Windows.Forms.Padding(4);
+            this.nameBox.Location = new System.Drawing.Point(101, 46);
             this.nameBox.Name = "nameBox";
-            this.nameBox.Size = new System.Drawing.Size(295, 22);
+            this.nameBox.Size = new System.Drawing.Size(222, 20);
             this.nameBox.TabIndex = 4;
             // 
             // modelBox
             // 
-            this.modelBox.Location = new System.Drawing.Point(135, 92);
-            this.modelBox.Margin = new System.Windows.Forms.Padding(4);
+            this.modelBox.Location = new System.Drawing.Point(101, 72);
             this.modelBox.Name = "modelBox";
-            this.modelBox.Size = new System.Drawing.Size(295, 22);
+            this.modelBox.Size = new System.Drawing.Size(222, 20);
             this.modelBox.TabIndex = 5;
             // 
             // manuBox
             // 
-            this.manuBox.Location = new System.Drawing.Point(135, 124);
-            this.manuBox.Margin = new System.Windows.Forms.Padding(4);
+            this.manuBox.Location = new System.Drawing.Point(101, 124);
             this.manuBox.Name = "manuBox";
-            this.manuBox.Size = new System.Drawing.Size(295, 22);
+            this.manuBox.Size = new System.Drawing.Size(222, 20);
             this.manuBox.TabIndex = 6;
             // 
             // zoneBox
             // 
-            this.zoneBox.Location = new System.Drawing.Point(135, 188);
-            this.zoneBox.Margin = new System.Windows.Forms.Padding(4);
+            this.zoneBox.Location = new System.Drawing.Point(101, 176);
             this.zoneBox.Name = "zoneBox";
-            this.zoneBox.Size = new System.Drawing.Size(295, 22);
+            this.zoneBox.Size = new System.Drawing.Size(222, 20);
             this.zoneBox.TabIndex = 9;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(19, 164);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(14, 150);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.Size = new System.Drawing.Size(45, 14);
             this.label1.TabIndex = 8;
             this.label1.Text = "Building";
             // 
             // buildingBox
             // 
-            this.buildingBox.Location = new System.Drawing.Point(135, 156);
-            this.buildingBox.Margin = new System.Windows.Forms.Padding(4);
+            this.buildingBox.Location = new System.Drawing.Point(101, 150);
             this.buildingBox.Name = "buildingBox";
-            this.buildingBox.Size = new System.Drawing.Size(295, 22);
+            this.buildingBox.Size = new System.Drawing.Size(222, 20);
             this.buildingBox.TabIndex = 7;
             // 
             // lblInstructions
             // 
-            this.lblInstructions.Location = new System.Drawing.Point(135, 22);
-            this.lblInstructions.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInstructions.Location = new System.Drawing.Point(101, 18);
             this.lblInstructions.Name = "lblInstructions";
-            this.lblInstructions.Size = new System.Drawing.Size(295, 21);
+            this.lblInstructions.Size = new System.Drawing.Size(221, 17);
             this.lblInstructions.TabIndex = 10;
             this.lblInstructions.Text = "Please enter machine details below.";
             this.lblInstructions.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // saveDetailBtn
             // 
-            this.saveDetailBtn.Location = new System.Drawing.Point(309, 220);
-            this.saveDetailBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.saveDetailBtn.Location = new System.Drawing.Point(232, 209);
             this.saveDetailBtn.Name = "saveDetailBtn";
-            this.saveDetailBtn.Size = new System.Drawing.Size(121, 28);
+            this.saveDetailBtn.Size = new System.Drawing.Size(91, 23);
             this.saveDetailBtn.TabIndex = 11;
             this.saveDetailBtn.Text = "Save Details";
             this.saveDetailBtn.UseVisualStyleBackColor = true;
@@ -157,10 +147,9 @@ namespace MEMS
             // 
             // clearBtn
             // 
-            this.clearBtn.Location = new System.Drawing.Point(135, 220);
-            this.clearBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.clearBtn.Location = new System.Drawing.Point(101, 209);
             this.clearBtn.Name = "clearBtn";
-            this.clearBtn.Size = new System.Drawing.Size(100, 28);
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
             this.clearBtn.TabIndex = 12;
             this.clearBtn.Text = "Clear";
             this.clearBtn.UseVisualStyleBackColor = true;
@@ -168,28 +157,43 @@ namespace MEMS
             // 
             // lblConfirm
             // 
-            this.lblConfirm.Location = new System.Drawing.Point(471, 9);
-            this.lblConfirm.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblConfirm.Location = new System.Drawing.Point(353, 7);
             this.lblConfirm.Name = "lblConfirm";
-            this.lblConfirm.Size = new System.Drawing.Size(287, 41);
+            this.lblConfirm.Size = new System.Drawing.Size(215, 33);
             this.lblConfirm.TabIndex = 14;
             this.lblConfirm.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // detailBox
             // 
             this.detailBox.FormattingEnabled = true;
-            this.detailBox.ItemHeight = 16;
-            this.detailBox.Location = new System.Drawing.Point(471, 60);
-            this.detailBox.Margin = new System.Windows.Forms.Padding(4);
+            this.detailBox.Location = new System.Drawing.Point(353, 49);
             this.detailBox.Name = "detailBox";
-            this.detailBox.Size = new System.Drawing.Size(287, 148);
+            this.detailBox.Size = new System.Drawing.Size(216, 121);
             this.detailBox.TabIndex = 15;
+            // 
+            // serialNumBox
+            // 
+            this.serialNumBox.Location = new System.Drawing.Point(100, 98);
+            this.serialNumBox.Name = "serialNumBox";
+            this.serialNumBox.Size = new System.Drawing.Size(222, 20);
+            this.serialNumBox.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 12);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Serial Number";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // AddMachineWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(788, 283);
+            this.ClientSize = new System.Drawing.Size(590, 285);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.serialNumBox);
             this.Controls.Add(this.detailBox);
             this.Controls.Add(this.lblConfirm);
             this.Controls.Add(this.clearBtn);
@@ -205,12 +209,15 @@ namespace MEMS
             this.Controls.Add(this.lblManu);
             this.Controls.Add(this.lblModel);
             this.Controls.Add(this.lblName);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AddMachineWindow";
             this.Text = "[MEMS] DetailedMachineWindow";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.TextBox serialNumBox;
 
         private System.Windows.Forms.ListBox detailBox;
 
