cb4719c928916c15f5bbd6f38145abc5e8d7094f
Lorenzo Murillo IV <Lorenzo.murillo01@student.csulb.edu>
2023-11-05 21:53:03 -0800

    Using Exists to check severity, will need to correct.

--- Model/Repository.cs
+++ Model/Repository.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using MEMS.Model;
+using MongoDB.Driver.Search;
 
 namespace MEMS
 {
@@ -50,7 +52,7 @@ namespace MEMS
         {
             return _collection.Find(filter);
         }
-
+        
         public IFindFluent<T, T> FindPage(FilterDefinition<T> filter, int skip, int limit)
         {
             return _collection.Find(filter).Skip(skip).Limit(limit);
