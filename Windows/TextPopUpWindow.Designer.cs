d8ab68a52d1e82382f7a50b017a5f9679d8d7f32
Lorenzo Murillo IV <Lorenzo.murillo01@student.csulb.edu>
2023-11-21 19:18:32 -0800

    Text Pop up Window UI Refactor

--- Windows/TextPopUpWindow.Designer.cs
+++ Windows/TextPopUpWindow.Designer.cs
@@ -29,24 +29,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextPopUpWindow));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(37, 19);
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(43, 22);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(522, 20);
+            this.textBox1.Size = new System.Drawing.Size(608, 16);
             this.textBox1.TabIndex = 0;
             // 
             // TextPopUpWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(571, 63);
+            this.ClientSize = new System.Drawing.Size(666, 73);
             this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "TextPopUpWindow";
+            this.Text = "Pop Up ";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
